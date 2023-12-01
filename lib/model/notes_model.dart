bd9aff1f7e274b384a44d2ae992ed8e85e305869
Nikita93029 <128574853+Nikita93029@users.noreply.github.com>
2023-12-02 00:52:08 +0530

    Update notes_model.dart

--- lib/model/notes_model.dart
+++ lib/model/notes_model.dart
@@ -1,9 +1,5 @@
 class Note {
   String id;
   String subtitle;
-  String title;
-  String time;
-  int image;
-  bool isDon;
-  Note(this.id, this.subtitle, this.time, this.image, this.title, this.isDon);
+  Note(this.id, this.subtitle);
 }
